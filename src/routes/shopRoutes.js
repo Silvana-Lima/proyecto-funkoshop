f46bbebc2c8fae8c0ebc963be4ec6e99c69d62db
silvana02 <silvanalima.89@hotmail.com>
2023-11-27 14:22:54 +0100

    add shopController

--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const shopControlers = require("./controlers/shopController");
 
-router.get("/", (req, res) => res.send("route for Shop view"));
-router.get("/item/:id", (req, res) =>
-  res.send(`route for find and retrieve a product from de ID: ${req.params.id}`)
-);
-router.post("/item/:id/add", (req, res) =>
-  res.send("route for add the current item to de shop cart")
-);
-router.get("/cart", (req, res) => res.send("route for Cart view"));
-router.post("/cart", (req, res) => res.send("route for go to checkout page"));
+router.get("/", shopControlers.shop);
+router.get("/item/:id", shopControlers.item);
+router.post("/item/:id/add", shopControlers.addItem);
+router.get("/cart", shopControlers.cart);
+router.post("/cart", shopControlers.checkoutCart);
 
 module.exports = router;

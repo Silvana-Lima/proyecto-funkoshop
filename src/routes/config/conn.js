9ccb7a0b11d859a188b9ad4e000b9b0eb486dba0
silvana02 <silvanalima.89@hotmail.com>
2023-12-09 19:25:49 +0100

    Install mysql2 dependency

---
+++ src/routes/config/conn.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2");
+require("dornev").config();
+
+const pool = mysql.createPool({
+  host: process.env.DBHOST,
+  user: process.env.DBUSER,
+  password: process.env.DBPASS,
+  database: process.env.DBNAME,
+  port: process.env.DBPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
